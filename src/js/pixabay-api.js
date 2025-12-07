7ce6c40898d9964a2a80898208414f8010a9fe63
Olha Brekhuntsova <olga.pistryak@gmail.com>
2025-12-07 20:46:58 +0100

    add request params inside request function

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,17 +4,17 @@ import { alertToast } from '../main';
 export const PAGE_SIZE = 15;
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-const params = {
-  key: '19987924-b7a96d980a74a373c9da34c4a',
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: true,
-  per_page: PAGE_SIZE,
-  q: '',
-};
 export async function getImagesByQuery(query, page) {
-  params.q = query;
-  params.page = page;
+  const params = {
+    key: '19987924-b7a96d980a74a373c9da34c4a',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: PAGE_SIZE,
+    q: query,
+    page,
+  };
+
   try {
     const res = await axios.get('', { params });
     return res.data;
